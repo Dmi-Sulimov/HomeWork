e67a9c07ccbea29c1878bf3aee85b108250865df
Dmitry <dmi-sulimov@rambler.ru>
2022-08-14 09:30:35 +0400

    Палиндром или нет

---
+++ Homework19/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+
+Console.WriteLine("Введите пятизначное число: ");
+int randomNumber = Convert.ToInt32(Console.ReadLine());
+
+int temp = randomNumber;
+int rev = 0;
+
+if (randomNumber > 2)
+{
+    while (randomNumber > 0)
+    {
+    int dig = randomNumber % 10;
+    rev = rev * 10 + dig;
+    randomNumber = randomNumber / 10;
+    }
+    if (temp == rev)
+    {
+    Console.WriteLine("Это палиндром!");
+    }
+    else
+    {
+    Console.WriteLine("Это не палиндром!");
+    }
+}
+
+
