7b37d58aec8269a78f823cd9b427dd1687f57899
Dmitry <dmi-sulimov@rambler.ru>
2022-08-21 14:38:55 +0400

    Массив из случайных чисел, количество элементов, значения которых лежат в отрезке [10, 99]

---
+++ Seminar5_35/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+Console.Clear();
+
+int[] arr = new int [13];
+
+void GetArray(int[] array)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Random rnd = new Random();
+        int value = rnd.Next(1000);
+        arr[i] = value;
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void NumArray(int[] array)
+{
+    int numExist = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {   
+        if (arr[i] > 9 && arr[i] < 100)
+        {  
+            numExist++;
+        }
+    }
+    if (numExist > 0)
+       {
+           Console.WriteLine($"Количество чисел: {numExist}");
+       }
+       else
+       {
+           Console.WriteLine("Чисел нет");
+       }
+}
+
+GetArray(arr);
+NumArray(arr);
